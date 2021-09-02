911a8c8e29f9b700e62852f7ae2b500a2c99e277
glazarashvili <giorgi.lazarashvili96@gmail.com>
2021-09-02 12:07:40 +0400

    added footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,55 @@
-import React from 'react'
+import items from './index.js'
+import classes from './Footer.module.css'
+
+import Template from '../../UI/Template/Template'
 
 const Footer = () => {
-  return <footer></footer>
+  const footerList = items.map(item => {
+    return (
+      <li>
+        <img src={item.image} />
+        <a target='_blank' href={item.link}>
+          {item.title}
+        </a>
+      </li>
+    )
+  })
+
+  return (
+    <footer className={classes.footer}>
+      <Template>
+        <ul className={classes['footer-items']}>
+          <li>
+            <a>Bookshop</a>
+          </li>
+          <li>
+            <a>Contact us</a>
+          </li>
+          <li>
+            <a>Useful sites</a>
+          </li>
+          <li>
+            <a>Help</a>
+          </li>
+        </ul>
+        <ul className={classes['footer-items']}>{footerList}</ul>
+        <ul className={classes['footer-items']}>
+          <li>
+            <a>Privacy Policy</a>
+          </li>
+          <li>
+            <a>Terms and Conditions</a>
+          </li>
+          <li>
+            <a>Delivery Options</a>
+          </li>
+          <li>
+            <a>Gift Cards</a>
+          </li>
+        </ul>
+      </Template>
+    </footer>
+  )
 }
 
 export default Footer
---
+++ src/components/Footer/index.js
@@ -0,0 +1,24 @@
+const items = [
+  {
+    title: 'facebook',
+    image: 'https://www.bookshop.ge/images/icons/facebook.png',
+    link: 'https://www.facebook.com/ebebookshop/',
+  },
+  {
+    title: 'twitter',
+    image: 'https://www.bookshop.ge/images/icons/instagram.png',
+    link: 'https://www.facebook.com/ebebookshop/',
+  },
+  {
+    title: 'instagram',
+    image: 'https://www.bookshop.ge/images/icons/twitter.png',
+    link: 'https://www.instagram.com/bookshop.ge/?igshid=1gz9c1ojth4e0',
+  },
+  {
+    title: 'youtube',
+    image: 'https://www.bookshop.ge/images/icons/youtube.png',
+    link: 'https://www.instagram.com/bookshop.ge/?igshid=1gz9c1ojth4e0',
+  },
+]
+
+export default items
